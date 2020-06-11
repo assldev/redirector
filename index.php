00d01cd55f637285eb13c0749fa6aef08566b9a2
akshitsingla <akshit.singla@yahoo.com>
2020-06-10 22:36:26 -0400

    Changing strategy to simulate

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php header("Location: " . $_GET["url"]); ?>
+<?php 
+// header("Location: " . $_GET["url"]); 
+// The above code probably did a server-side redirect, and hence lead was shown as "organic" in Adobe Analytics
+
+header("Location: index.html"); 
+?>

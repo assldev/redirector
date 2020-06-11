b56b514c063cf2ebeb4733db3fd8bdab62c0524e
akshitsingla <akshit.singla@yahoo.com>
2020-06-10 22:46:13 -0400

    silly fix

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
-<?php 
-// header("Location: " . $_GET["url"]); 
-// The above code probably did a server-side redirect, and hence lead was shown as "organic" in Adobe Analytics
-
-header('Location: /index.html'); 
-?>
+<?php header('Location: /index.html'); ?>

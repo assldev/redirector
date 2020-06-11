bfd073a4b4927b9ced43fa0715caccaf0c86a639
akshitsingla <akshit.singla@yahoo.com>
2020-06-10 22:43:20 -0400

    Minor php syntax fix

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 // header("Location: " . $_GET["url"]); 
 // The above code probably did a server-side redirect, and hence lead was shown as "organic" in Adobe Analytics
 
-header("Location: /index.html"); 
+header('Location: /index.html'); 
 ?>
